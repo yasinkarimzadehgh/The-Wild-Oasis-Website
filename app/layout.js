d02abe505940d68a4fb0054ea108f77a98202713
Yasin Karimzadeh <yasinkarimzadehgh@gmail.com>
2024-09-12 16:07:10 +0330

    modified

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,7 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 
 import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -22,15 +21,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
+        <Header />
 
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
