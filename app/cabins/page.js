3700fdf70b36816d6ae36f5f144d6e7088cdb201
Yasin Karimzadeh <yasinkarimzadehgh@gmail.com>
2024-09-17 18:59:39 +0330

    modified

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,15 +1,13 @@
-import Counter from "@/app/_components/Counter";
 import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "../_lib/data-service";
+import CabinList from "../_components/CabinList";
+import { Suspense } from "react";
+import Spinner from "../_components/Spinner";
 
 export const metadata = {
   title: "Cabin",
 };
 export default async function Page() {
-  console.log("Starting...");
-  const cabins = await getCabins();
-  console.log(cabins);
-
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -24,13 +22,9 @@ export default async function Page() {
         Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -10,8 +10,8 @@ const josefin = Josefin_Sans({
 
 export const metadata = {
   title: {
-    template: "%s / The Wild Oasis",
-    default: "Welcome / The Wild Oasis",
+    template: "%s | The Wild Oasis",
+    default: "Welcome | The Wild Oasis",
   },
   description:
     "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountainous and dark forests",
